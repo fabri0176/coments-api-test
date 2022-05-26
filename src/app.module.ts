e5e38470b56ecf132ac6e67209dc15257e0190a8
Fabricio <fabri0176@gmail.com>
2022-05-25 20:31:34 -0500

    Configuracion base de datos postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CommentsModule } from './modules/comments/comments.module';
 
+
 @Module({
-  imports: [CommentsModule],
+  imports: [CommentsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'admin2022',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true //Solo desarrollo
+  })],
 })
-export class AppModule {}
+export class AppModule { }
