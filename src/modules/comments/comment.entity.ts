626760dbcbb66f4a6e2d74f2aabe31084fdef68a
Fabricio <fabri0176@gmail.com>
2022-05-30 10:57:44 -0500

    Configurando entidad base de datos

--- src/modules/comments/comment.entity.ts
+++ src/modules/comments/comment.entity.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Comment {
-    id: string;
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
     message: string;
 }
